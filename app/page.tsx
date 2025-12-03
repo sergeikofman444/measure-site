656b557c554409853e31edd8006ac004e58eb194
sergeikofman-dealmaker <sergeikofman444@gmail.com>
2025-12-03 13:17:39 -0500

    format

--- app/page.tsx
+++ app/page.tsx
@@ -6,37 +6,34 @@ import ProjectTile from "@/components/ProjectTile";
 import ProjectFilter from "@/components/ProjectFilter";
 
 export default function Home() {
-  const [selectedTypes, setSelectedTypes] = useState<Set<ProjectType>>(new Set());
+  const [selectedTypes, setSelectedTypes] = useState<Set<ProjectType>>(
+    new Set(),
+  );
 
   function toggleType(type: ProjectType | "all") {
-  const newSelected = new Set(selectedTypes);
+    const newSelected = new Set(selectedTypes);
 
-  if (type === "all") {
-    newSelected.clear();
-  } else {
-    if (newSelected.has(type)) {
-      newSelected.delete(type);
+    if (type === "all") {
+      newSelected.clear();
     } else {
-      newSelected.add(type);
+      if (newSelected.has(type)) {
+        newSelected.delete(type);
+      } else {
+        newSelected.add(type);
+      }
+      newSelected.delete("all" as ProjectType);
     }
-    newSelected.delete("all" as ProjectType);
+
+    setSelectedTypes(newSelected);
   }
 
-  setSelectedTypes(newSelected);
-}
-
   const visibleProjects = Object.values(projects).filter((p) =>
-    selectedTypes.size === 0
-      ? true
-      : p.type.some((t) => selectedTypes.has(t))
+    selectedTypes.size === 0 ? true : p.type.some((t) => selectedTypes.has(t)),
   );
 
   return (
     <>
-      <ProjectFilter
-        selectedTypes={selectedTypes}
-        toggleType={toggleType}
-      />
+      <ProjectFilter selectedTypes={selectedTypes} toggleType={toggleType} />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 p-4">
         <AnimatePresence>
           {visibleProjects.map((project) => (
--- components/ProjectFilter.tsx
+++ components/ProjectFilter.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { projectTypes, ProjectType } from "@/constants/projects"
+import { projectTypes, ProjectType } from "@/constants/projects";
 import { X } from "lucide-react";
 
 type ProjectFilterProps = {
@@ -8,57 +8,60 @@ type ProjectFilterProps = {
   toggleType: (type: ProjectType | "all") => void;
 };
 
-export default function ProjectFilter ({ selectedTypes, toggleType }: ProjectFilterProps) {
-    const [isFilterOpen, setIsFilterOpen] = useState(false);
+export default function ProjectFilter({
+  selectedTypes,
+  toggleType,
+}: ProjectFilterProps) {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
 
-    return(
-        <div className="relative flex justify-end px-4">
-            <button
-              onClick={() => setIsFilterOpen(!isFilterOpen)}
-              aria-label="Toggle menu"
-            >
-                <div className="flex gap-2 items-center mb-4">
-                    <span className="font-semibold">
-                        {selectedTypes.size === 0
-                        ? "all projects"
-                        : Array.from(selectedTypes).join(", ")}
-                    </span>
-                </div>
-            </button>
+  return (
+    <div className="relative flex justify-end px-4">
+      <button
+        onClick={() => setIsFilterOpen(!isFilterOpen)}
+        aria-label="Toggle menu"
+      >
+        <div className="flex gap-2 items-center mb-4">
+          <span className="font-semibold">
+            {selectedTypes.size === 0
+              ? "all projects"
+              : Array.from(selectedTypes).join(", ")}
+          </span>
+        </div>
+      </button>
 
-           <div
-            className={`
+      <div
+        className={`
                 absolute right-0 top-full mt-2 mr-4
                 z-50 w-30
                 transition-opacity duration-300 rounded-md bg-black
                 ${isFilterOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}
-            `} 
-            >
-                <div>
-                    <button
-                        className={`px-2 py-1 rounded ${
-                        selectedTypes.size === 0 ? "font-semibold" : ""
-                        }`}
-                        onClick={() => toggleType("all" as ProjectType)}
-                    >
-                        all projects
-                    </button>
-                </div>
-
-                {projectTypes.map((type) => (
-                    <div>
-                        <button
-                        key={type}
-                        className={`px-2 py-1 rounded ${
-                            selectedTypes.has(type) ? "font-semibold" : ""
-                        }`}
-                        onClick={() => toggleType(type)}
-                        >
-                            {type}
-                        </button>
-                    </div>
-                ))}
-            </div>
+            `}
+      >
+        <div>
+          <button
+            className={`px-2 py-1 rounded ${
+              selectedTypes.size === 0 ? "font-semibold" : ""
+            }`}
+            onClick={() => toggleType("all" as ProjectType)}
+          >
+            all projects
+          </button>
         </div>
-    )
+
+        {projectTypes.map((type) => (
+          <div>
+            <button
+              key={type}
+              className={`px-2 py-1 rounded ${
+                selectedTypes.has(type) ? "font-semibold" : ""
+              }`}
+              onClick={() => toggleType(type)}
+            >
+              {type}
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
